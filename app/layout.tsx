eaf509c4617883120c58ac1e7a1779ecb224c5b2
Naveen <naveensai.alapati6@gmail.com>
2025-11-05 10:43:46 -0600

    updated layout.tsx, globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
-import {Loader}  from "@/components/index";
+import { Loader } from "@/components";
 import { Suspense } from "react";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +12,8 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     locale: "en_US",
-    title:
-      "Naveen Sai",
-    description:
-      "Explore the portfolio of Naveen Sai",
+    title: "Naveen Sai",
+    description: "Explore the portfolio of Naveen Sai",
     images: [
       {
         url: "/og-image.webp",
@@ -23,10 +21,8 @@ export const metadata: Metadata = {
       },
     ],
   },
-  title:
-    "Naveen Sai Alapati | Full Stack Developer | JPMC | Kansas,USA",
-  description:
-    "Explore the portfolio of Naveen Sai",
+  title: "Naveen Sai Alapati | Full Stack Developer | JPMC | Kansas, USA",
+  description: "Explore the portfolio of Naveen Sai",
   keywords:
     "Naveen Sai Alapati, ReactJS, Angular, Java, SpringBoot, Node.js, Javascript, Typescript, Material UI, TailwindCSS, Web Development, Portfolio, React developer, Next js developer, USA",
   robots:
@@ -46,16 +42,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-black-100 text-white`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
-          <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden">
+          <main className="relative flex flex-col min-h-screen w-full overflow-x-hidden">
             <Suspense fallback={<Loader />}>{children}</Suspense>
-            
           </main>
         </ThemeProvider>
         <Analytics />
